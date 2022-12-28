5d111efd34a5168232855acf04b6007066d3d34a
Gabriele Callegari <g.callegari@itsrizzoli.it>
2022-12-28 15:39:27 +0100

    updated svuota carrello

--- app/src/main/java/it/itsar/amazon_redo/fragments/CartFragment.java
+++ app/src/main/java/it/itsar/amazon_redo/fragments/CartFragment.java
@@ -2,6 +2,9 @@ package it.itsar.amazon_redo.fragments;
 
 import static it.itsar.amazon_redo.http.model.Prodotto_dettaglio.carrello;
 
+import android.app.AlertDialog;
+import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,6 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +20,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import it.itsar.amazon_redo.Adapter.CarrelloAdapter;
-import it.itsar.amazon_redo.Adapter.ListaAdapter;
+import it.itsar.amazon_redo.MainActivity;
 import it.itsar.amazon_redo.R;
 
 public class CartFragment extends Fragment {
@@ -49,5 +53,27 @@ public class CartFragment extends Fragment {
         svuotaCarrello.setVisibility(carrello.size()==0 ? View.GONE : View.VISIBLE);
         carrelloVuoto.setVisibility(carrello.size()==0 ? View.VISIBLE : View.GONE);
 
+
+        svuotaCarrello.setOnClickListener(v -> {
+            carrello.clear();
+            Log.d("CARRELLO SIZE", "onViewCreated: "+carrello.size());
+
+            DialogInterface.OnClickListener listener = (dialog, which) -> {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        carrelloView.setVisibility(View.GONE);
+                        acquista.setVisibility(View.GONE);
+                        svuotaCarrello.setVisibility(View.GONE);
+                        carrelloVuoto.setVisibility(View.VISIBLE);
+                        break;
+                }
+            };
+            new AlertDialog.Builder(getContext()).setTitle("Sei sicuto di voler svuotare il carrello?")
+                    .setPositiveButton("SVUOTA",listener)
+                    .setNegativeButton("ANNULLA",listener)
+                    .show();
+        });
+
     }
+
 }
